e388322b0c3371ded8d75c26e88530dacd405448
Eoin Boylan <jb.eoin@gmail.com>
2023-12-06 22:50:15 +0000

    init

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,19 @@
-
-
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("Starting e3-mini");   
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:8001").await?;
+
+    loop {
+        let (mut steam, _addr) = listener.accept().await?;
+
+        tokio::spawn(async move {
+            let mut buffer = [0;1024];
+
+            match steam.read() {
+                
+            }
+    }
+        
+
+    Ok(())
 }
